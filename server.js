c12ff43eb5e3f8f399e6a68696f606866db5732f
Miguel Peláez Medina <miguel@3in-tech.com>
2020-11-16 00:11:59 +0100

    idk

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/anima-spell-search'));
+app.use(express.static(__dirname + '/dist/AnimaSpellSearch'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/anima-spell-search/src/index.html'));});
+'/dist/AnimaSpellSearch/index.html'));});
 app.listen(process.env.PORT || 8080);
